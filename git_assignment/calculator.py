02d73a60db33e7a950d9edac8aef36caaac17439
fancykejriwal <ms.fkejriwal@outlook.com>
2025-12-07 22:57:35 +0530

    Added CalculatorPlus with square root feature

--- git_assignment/calculator.py
+++ git_assignment/calculator.py
@@ -1,27 +1,31 @@
 import math
 
 class Calculator:
-    def add( self , a, b) :
+    def add(self, a, b):
         return a + b
-    def substract( self, a,b) :
+
+    def subtract(self, a, b):
         return a - b
-    def multiply( self , a,b) :
+
+    def multiply(self, a, b):
         return a * b
-    def divide(self , a,b) :
-        if b ==0 :
-            raise ValueError("Cannot divide by zero")
-        return a/b
-    def square_root(self ,a) :
-        if a < 0 :
+
+    def divide(self, a, b):
+        return a / b
+
+    def square_root(self, a):
+        if a < 0:
             raise ValueError("Cannot compute square root of negative number")
         return math.sqrt(a)
-    if __name__ == " __main__" :
-        Calculator = Calculator()
 
-        number1= 16
-        number2 = 4
-        print("Addition:", Calculator.add(number1,number2))
-        print("Subtraction:",Calculator.substract(number1,number2))
-        print("Multiplication:",Calculator.multiply(number1,number2))
-        print("Division:",Calculator.divide(number1,number2))
-        print("Square Root:",Calculator.sqaure_root(number1))
+if __name__ == "__main__":
+    calculator = Calculator()
+
+    number1 = 16
+    number2 = 4
+
+    print("Addition:", calculator.add(number1, number2))
+    print("Subtraction:", calculator.subtract(number1, number2))
+    print("Multiplication:", calculator.multiply(number1, number2))
+    print("Division:", calculator.divide(number1, number2))
+    print("Square Root:", calculator.square_root(number1))
